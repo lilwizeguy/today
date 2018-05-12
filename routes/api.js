4adeb0212bfd6e2cbbba75b048050cede52750ee
Manny1995 <immanuelda@gmail.com>
2018-05-11 20:59:40 -0700

    news api now works

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,12 @@
+// Immanuel Amirtharaj
+// api.js
 
 const express = require('express');
 const request = require('request');
 
 const StocksAPI = require('../services/stocks');
 const WeatherAPI = require('../services/weather');
+const NewsAPI = require('../services/news')
 
 const responseFormatter = require('../formatters/response-formatter')
 const router = express.Router();
@@ -16,34 +19,30 @@ router.get('/weather', (req, res, next) => {
         "lon" : req.query.lon,
     };
 
-    console.log(coordinates);
 
     const weatherUrl = WeatherAPI.weatherUrl(coordinates.lat, coordinates.lon);
-    console.log(weatherUrl)
 
     request.get(weatherUrl, (error, response, body) => {
         const parsedBody = WeatherAPI.parseResponse(body);
         if (error) {
-            res.json(responseFormatter.baseReponse(500, error, null));
+            res.json(responseFormatter.failedResponse(500, error));
         }
         else {
             res.json(responseFormatter.successfulResponse(parsedBody));
         }
     });
-    
-
 });
 
 
 router.get('/stocks', (req, res, next) => {
 
-    const stocks = ['AAPL', 'GOOG'];
+    const stocks = ['AAPL', 'GOOG', 'FB'];
     const batchUrl = StocksAPI.batchUrl(stocks);
 
     request.get(batchUrl, null, (error, response, body) => { 
         const parsedBody = StocksAPI.parseResponse(body);
         if (error) {
-            res.json(responseFormatter.baseReponse(500, error, null));
+            res.json(responseFormatter.failedResponse(500, error));
         }
         else {
             res.json(responseFormatter.successfulResponse(parsedBody));
@@ -51,12 +50,13 @@ router.get('/stocks', (req, res, next) => {
     });
 });
 
-router.get('/reddit', (req, res, next) => {
 
-    res.json('Hit reddit');
-});
+router.get('/news', (req, res, next) => {
+    
+    NewsAPI.getHeadlines((newsResp) => {
 
-router.get('/hacker-news', (req, res, next) => {
+        res.json(responseFormatter.successfulResponse(newsResp));
+    });
 
 });
 
--- services/news.js
+++ services/news.js
@@ -1,2 +1,48 @@
 // 7240baf51c3d4bc49ac52e7563e0e263
 // https://newsapi.org/v2/top-headlines?country=us&apiKey=7240baf51c3d4bc49ac52e7563e0e263
+
+
+
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('7240baf51c3d4bc49ac52e7563e0e263');
+
+
+
+function getHeadlines(callback) {
+
+	newsapi.v2.topHeadlines({
+        country : 'us',
+        language : 'en',
+    }).then(response => {
+        const filteredResponse = parseResponse(response);
+        callback(filteredResponse);
+    });
+    
+}
+
+function parseResponse(response) {
+
+
+    // const parsedJson = JSON.parse(response);
+
+    const articles = response["articles"];
+    console.log(articles);
+    
+    const articleArr = articles.map((value) => {
+        const parsedVal = {
+            "sourceName" : value["source"]["name"],
+            "title" : value["title"],
+            "description" : value["description"],
+            "url" : value["url"],
+            "imageUrl" : value["urlToImage"],
+            "timestamp" : value["publishedAt"],
+        };
+
+        return parsedVal;
+    });
+
+    return articleArr;
+}
+
+
+module.exports.getHeadlines = getHeadlines;
