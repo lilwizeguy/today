3172ce7100846cfe84a941516d6b72c6ed0a3947
Manny1995 <immanuelda@gmail.com>
2018-05-17 01:00:11 -0700

    added pell editor for notes

--- today-frontend/src/App.js
+++ today-frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Editor from 'react-pell';
 
 import {getStocks, getWeather, getNews}  from './api'
 
@@ -40,16 +41,40 @@ class StocksCard extends Component {
     }
 
     const stocks = this.state.dataSource.map(value => {
-      return <li>{value.symbol}</li>;
+      return <li class="collection-item">{value.symbol} {value.price}</li>;
     });
 
 
     return (
-      <div>
-        <h2>Stocks</h2>
-        <ul>
-          {stocks}
+      <div class="row">
+        <div class="col s12 m3"></div>
+        <div class="col s12 m6 ">
+        <ul class="collection with-header z-depth-1">
+            <li class="collection-header"><h4>Stocks</h4></li>
+            {stocks}
         </ul>
+        </div>
+        <div class="col s12 m3"></div>
+      </div>
+    );
+  }
+}
+
+class NotesCard extends Component {
+
+
+  render() {
+    return (
+      <div class="row">
+        <div class="col s12 m3"></div>
+        <div class="col s12 m6 ">
+          <ul class="collection with-header z-depth-1">
+            <li class="collection-header"><h4>Notes</h4></li>
+      {/* <Editor onChange={this.handleChange.bind(this)} /> */}
+            <Editor />
+          </ul>
+        </div>
+        <div class="col s12 m3"></div>
       </div>
     );
   }
@@ -59,8 +84,17 @@ class WeatherCard extends Component {
 
   render() {
     return (
-      <div>
-        <h2>Weather</h2>
+      <div class="row">
+        <div class="col s12 m3"></div>
+        <div class="col s12 m6 ">
+        <ul class="collection with-header z-depth-1">
+            <li class="collection-header"><h4>Weather</h4></li>
+            <li class="collection-item">Temperature</li>
+            <li class="collection-item">Air Pressure</li>
+
+        </ul>
+        </div>
+        <div class="col s12 m3"></div>
       </div>
     )
   }
@@ -103,15 +137,22 @@ class NewsCard extends Component {
 
 
     const stories = this.state.dataSource.map(value => {
-        return <li>{value.title}</li>;
+        return <li class="collection-item left-align">
+                <a href={value.url}>{value.title}</a>
+                <p>{value.sourceName}</p>
+                </li>;
     });
 
     return (
-      <div>
-        <h2>News</h2>
-        <ul>
+      <div class="row">
+        <div class="col s12 m3"></div>
+        <div class="col s12 m6 ">
+        <ul class="collection with-header z-depth-1">
+          <li class="collection-header"><h4>News</h4></li>
           {stories}
         </ul>
+        </div>
+        <div class="col s12 m3"></div>
       </div>
     )
   }
@@ -122,6 +163,7 @@ class App extends Component {
     return (
       <div className="App">
 
+        <NotesCard />
         <StocksCard />
         <WeatherCard />
         <NewsCard />
